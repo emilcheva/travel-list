8c4f1184733c2f4b2017760cf154c51613f960b6
emilcheva <evgenia.milcheva@hotmail.com>
2024-11-30 14:06:31 +0200

    Implement sorting

--- src/components/item-list/ItemList.jsx
+++ src/components/item-list/ItemList.jsx
@@ -1,10 +1,41 @@
+import Select from "react-select";
 import Item from "./Item";
+import { useState } from "react";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({
   items,
   handleRemoveItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+
+    return;
+  });
+
   return (
     <ul className="item-list">
       {items.length === 0 && (
@@ -13,7 +44,17 @@ export default function ItemList({
         </div>
       )}
 
-      {items.map(({ name, id, packed }) => (
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+            onChange={(option) => setSortBy(option.value)}
+          />
+        </section>
+      )}
+
+      {sortedItems.map(({ name, id, packed }) => (
         <Item
           id={id}
           packed={packed}
